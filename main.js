6729a3b1d5d8f37f390e9b104622f0a3231a2a19
SiddharthShyniben <siddharth.muscat@gmail.com>
2022-04-06 11:26:01 +0530

    Move codemirror script to htmlpage

--- main.js
+++ main.js
@@ -1,50 +1,3 @@
-const codemirror = CodeMirror(document.querySelector('div#code'), {
-	value: 'print("FizzBuzz")',
-	mode: 'javascript',
-	theme: 'material-darker',
-	autoCloseBrackets: true,
-	lineNumbers: true,
-})
-
-const getFinishDate = () => {
-	const finishDate = new Date();
-	finishDate.setDate(finishDate.getDate() + 1)
-	finishDate.setHours(5);
-	finishDate.setMinutes(30);
-	finishDate.setMilliseconds(0);
-	return finishDate;
-}
-
-let finishDate = getFinishDate();
-
-const remainingTime = finishDate - Date.now();
-document.querySelector('#timestring').innerHTML = msToTime(remainingTime);
-setInterval(() => {
-	if (finishDate - Date.now() <= 0) {
-		finishDate = getFinishDate();
-	}
-
-	const remainingTime = finishDate - Date.now();
-	document.querySelector('#timestring').innerHTML = msToTime(remainingTime);
-}, 60000);
-
-function padTo2Digits(num) {
-	return num.toString().padStart(2, '0');
-}
-
-function msToTime(milliseconds) {
-	let seconds = Math.floor(milliseconds / 1000);
-	let minutes = Math.floor(seconds / 60);
-	let hours = Math.floor(minutes / 60);
-
-	seconds = seconds % 60;
-	minutes = seconds >= 30 ? minutes + 1 : minutes;
-	minutes = minutes % 60;
-	hours = hours % 24;
-
-	return `${padTo2Digits(hours)}h ${padTo2Digits(minutes)}m`;
-}
-
 // auth
 let auth0 = null;
 let authConfig = {
@@ -143,4 +96,14 @@ window.onload = async () => {
 
 	loginButton.addEventListener('click', login);
 	logoutButton.addEventListener('click', logout);
+
+	// profile page
+	const profileDiv = document.querySelector('#profile-page');
+	if (profileDiv) {
+		const user = await auth0.getUser();
+		
+		const nickname = profileDiv.querySelector('#nickname');
+		nickname.innerHTML = user.nickname;
+	}
 }
+
